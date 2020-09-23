c64868effd2cee8261a1d43daa25b0f4f5b0911b
MegLip <70174916+MegLip@users.noreply.github.com>
2020-09-24 01:29:03 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -21,13 +21,11 @@ print("Podaj działanie, posługując się odpowiednią liczbą:" '\n' "1 Dodawa
 choice = input("Podaj numer działania (1,2,3,4): ")
 
 if __name__ == "__main__":
-
     if choice == "1":
         num1 = float(input ("Podaj liczbę nr 1: "))
         num2 = float(input ("Podaj liczbę nr 2: "))
         print("Podaj następne argumenty lub wciśnij k by zakończyć")
-        args = []
-             
+        args = []   
         while True:
             arg = input("Podaj kolejną liczbę lub k: ")                              #3 give more than 2 arguments
             if arg == "k":
@@ -50,13 +48,11 @@ if __name__ == "__main__":
         num2 = float(input ("Podaj liczbę nr 2: "))
         print("Podaj następne argumenty lub wciśnij k by zakończyć")
         args = [] 
-       
         while True:
             arg = input("Podaj kolejną liczbę lub k: ")                        #5 give more than 2 arguments
             if arg == "k":
                 break
             args.append(arg)
-
         #operations = {
         #    "add": add,
         #    "multiply": multiply
@@ -75,3 +71,17 @@ if __name__ == "__main__":
         logging.info(f"Mnożę {num1} i {num2} i {x}")
         print(f"Mnożę", num1, "i", num2, "i", (x))
         print(f"Wynik mnożenia to:", y)
+        
+    elif choice in ("2", "4"):
+        num1 = float(input ("Podaj liczbę nr 1: "))
+        num2 = float(input ("Podaj liczbę nr 2: "))
+        print(f"Sprawdzam, czy podane wartości są liczbami:", {type(num1)}, {type(num2)})    #7 check the given value is a number
+        if choice == '2':
+            logging.info(f"Odejmuję {num1} i {num2}")
+            print(f"Odejmuję", num1, "i", num2)
+            print(f"Wynik odejmowania to:", substract(num1, num2))
+        elif choice == '4':
+            logging.info(f"Dzielę {num1} i {num2}")        
+            print(f"Dzielę", num1, "i", num2)
+            print(f"Wynik dzielenia to:", (round(divide(num1,num2),2)))
+    else : print ("Niepoprawna wartość! Wybierz ponownie spośród działań (1,2,3,4)")
