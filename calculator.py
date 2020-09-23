ac702a26ff785a060d9072350196364e70deb906
MegLip <70174916+MegLip@users.noreply.github.com>
2020-09-24 01:21:05 +0200

    more than 2 arg and check value is a number

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import logging
 
-def add (num1, num2):               #1 def function
+def add (num1, num2):                              #1 def function
     return num1 + num2
 def add (num1, num2, *args):               
     return num1 + num2 + sum(args)  
@@ -17,5 +17,61 @@ def multiply (num1, num2, *args):
 def divide (num1, num2):
     return num1/num2
 
-print("Podaj działanie, posługując się odpowiednią liczbą:" '\n' "1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")        #2 choice operation
+print("Podaj działanie, posługując się odpowiednią liczbą:" '\n' "1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")            #2 choice operation
 choice = input("Podaj numer działania (1,2,3,4): ")
+
+if __name__ == "__main__":
+
+    if choice == "1":
+        num1 = float(input ("Podaj liczbę nr 1: "))
+        num2 = float(input ("Podaj liczbę nr 2: "))
+        print("Podaj następne argumenty lub wciśnij k by zakończyć")
+        args = []
+             
+        while True:
+            arg = input("Podaj kolejną liczbę lub k: ")                              #3 give more than 2 arguments
+            if arg == "k":
+                break
+            args.append(int(arg))
+        #operations = {
+        #    "add": add,
+        #    "multiply": multiply
+        #}
+        #op = input("Co będziemy zrobić? (add, multiply)")
+        #result = operations[op](num1, num2, *args)
+        result = add(num1, num2, *args)
+        print(f"Sprawdzam, czy podane wartości są liczbami:", {type(num1)}, {type(num2)}, {type([args])})          #4 check the given value is a number
+        logging.info(f"Dodaję {num1} i {num2} i {sum(args)}")
+        print(f"Dodaję", num1, "i", num2, "i", float(sum(args)))
+        print("Wynik dodawania to:", add(num1, num2, sum(args)))
+
+    elif choice == "3":
+        num1 = float(input ("Podaj liczbę nr 1: "))
+        num2 = float(input ("Podaj liczbę nr 2: "))
+        print("Podaj następne argumenty lub wciśnij k by zakończyć")
+        args = [] 
+       
+        while True:
+            arg = input("Podaj kolejną liczbę lub k: ")                        #5 give more than 2 arguments
+            if arg == "k":
+                break
+            args.append(arg)
+
+        #operations = {
+        #    "add": add,
+        #    "multiply": multiply
+        #}
+        #op = input("Co będziemy zrobić? (add, multiply)")
+
+        x = 1
+        for num in args:
+            x = x * float(num)
+            y = x * num2 * num1
+
+        #result = operations[op](num1, num2, y)
+        result = multiply(num1, num2, y)
+        print(f"Sprawdzam, czy podane wartości są liczbami:", {type(num1)}, {type(num2)}, {type(y)})          #6 check the given value is a number
+        
+        logging.info(f"Mnożę {num1} i {num2} i {x}")
+        print(f"Mnożę", num1, "i", num2, "i", (x))
+        print(f"Wynik mnożenia to:", y)
